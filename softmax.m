f5bc4199e535895818484a5fc3664e20f7039ffa
shawns0107 <shawns0107@gmail.com>
2016-09-19 17:09:56 -0400

    Create softmax.m

---
+++ softmax.m
@@ -0,0 +1,10 @@
++function out = softmax(K, dim)
+ 
+ +s = ones (1, ndims(K));
+ +s(dim) = size (K, dim);
+ +
+ +maxK = max(K, [], dim);
+ +expK = exp(K - repmat(maxK, s));
+ +out = expK ./ repmat (sum(expK, dim), s);
+ 
+ +end
